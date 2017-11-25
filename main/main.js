ac4f3982e322c1c9b2f6ecd62031fefcfa3e2a32
youthZ <552099787@qq.com>
2017-11-25 14:52:37 +0000

    finished

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(miles,waitMinute) {
+    var sumFee = 0;
+	if(miles <= 2){
+		sumFee += 6 + parseInt((waitMinute * 0.25).toFixed(0));
+	}
+
+	if(miles > 2 && miles <= 8){
+		sumFee += 6 + parseInt((waitMinute * 0.25).toFixed(0)) + parseInt(((miles-2) * 0.8).toFixed(0));
+	}
+
+	if(miles > 8 ){
+		sumFee = 6 + parseInt((waitMinute * 0.25).toFixed(0)) + parseInt((6 * 0.8).toFixed(0)) + parseInt(((miles-8) * 1.2).toFixed(0));
+	}
+
+	return sumFee;
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,27 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+	it('should return two miles and NO waitMinute fee',function(){
+		let miles = 2;
+		let waitMinute = 2;
+		let sumFee = main(miles,waitMinute);
+		let expected = 7;
+		expect(sumFee).toEqual(expected);
+	});
+	
+	it('should return more than two miles and less than 8 miles fee',function(){
+		let miles = 6;
+		let waitMinute = 0;
+		let sumFee = main(miles,waitMinute);
+		let expected = 9;
+		expect(sumFee).toEqual(expected);
+	});
+
+	it('should return more than 8 miles fee',function(){
+		let miles = 10;
+		let waitMinute = 10;
+		let sumFee = main(miles,waitMinute);
+		let expected = 16;
+		expect(sumFee).toEqual(expected);
+	});
 });
